976d4b72f295bea3643864e996b3b0e8f72ae76e
Mansoor Siddeeq <msiddeeq@msiddeeq-ltm1.internal.salesforce.com>
2017-01-19 18:02:24 -0500

    updated everything

--- build/js/tableau.js
+++ build/js/tableau.js
@@ -2,16 +2,16 @@
  * Created by kedouard on 1/15/2017.
  */
 function initializeViz() {
-    var vizDiv = document.getElementById("myViz");
+    var vizDiv = document.getElementById("v1");
     var url = "https://public.tableau.com/views/guard-eim-overview-1/db-natl-vs-ga";
     var options = {
-        width: vizDiv.offsetWidth,
+        width: '900px',
         height: '800px',
         hideTabs: true,
         hideToolbar: true,
         onFirstInteractive: function () {
             workbook = viz.getWorkbook();
-            activeSheet = workbook.activateSheetAsync("");
+            activeSheet = workbook.activateSheetAsync("db-natl-vs-ga");
         }
     };
     viz = new tableau.Viz(vizDiv, url, options);
@@ -19,7 +19,66 @@ function initializeViz() {
 
 $(initializeViz);
 
-//on.click() {
-//    workbook.activateSheetAsync("GDP per capita map");
-//}
-// http://onlinehelp.tableau.com/samples/en-us/js_api/tutorial.htm
+$("#b2").click(function(){
+    var vizDiv2 = document.getElementById("v2");
+    var url = "https://public.tableau.com/views/guard-eim-overview-2-contractors1_V1/DODContractSpending";
+    var options = {
+        width: '900px',
+        height: '800px',
+        hideTabs: true,
+        hideToolbar: true,
+        onFirstInteractive: function () {
+            workbook = viz.getWorkbook();
+            activeSheet = workbook.activateSheetAsync("DOD Contract Spending");
+        }
+    };
+    viz = new tableau.Viz(vizDiv2, url, options);
+});
+
+$("#b3").click(function(){
+    var vizDiv2 = document.getElementById("v3");
+    var url = "https://public.tableau.com/views/guard-eim-overview-3-installations/Dashboard1";
+    var options = {
+        width: '900px',
+        height: '800px',
+        hideTabs: true,
+        hideToolbar: true,
+        onFirstInteractive: function () {
+            workbook = viz.getWorkbook();
+            activeSheet = workbook.activateSheetAsync("Dashboard1");
+        }
+    };
+    viz = new tableau.Viz(vizDiv2, url, options);
+});
+
+$("#b4").click(function(){
+    var vizDiv2 = document.getElementById("v4");
+    var url = "https://public.tableau.com/views/guard-eim-overview-dynamics/Dashboard1";
+    var options = {
+        width: '900px',
+        height: '800px',
+        hideTabs: true,
+        hideToolbar: true,
+        onFirstInteractive: function () {
+            workbook = viz.getWorkbook();
+            activeSheet = workbook.activateSheetAsync("Dashboard1");
+        }
+    };
+    viz = new tableau.Viz(vizDiv2, url, options);
+});
+
+$("#b1_2").click(function(){
+    var vizDiv2 = document.getElementById("v1_2");
+    var url = "https://public.tableau.com/views/guard-eim-overview-1/db-ga-regions-counties";
+    var options = {
+        width: '900px',
+        height: '800px',
+        hideTabs: true,
+        hideToolbar: true,
+        onFirstInteractive: function () {
+            workbook = viz.getWorkbook();
+            activeSheet = workbook.activateSheetAsync("db-ga-regions-counties");
+        }
+    };
+    viz = new tableau.Viz(vizDiv2, url, options);
+});
